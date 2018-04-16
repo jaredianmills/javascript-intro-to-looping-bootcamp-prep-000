fabc5a8c196325c0df7989eef5a342f372197556
jaredianmills <jaredianmills@gmail.com>
2018-04-16 20:23:13 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -22,6 +22,6 @@ function maybeTrue() {
 
 function doWhileLoop(array) {
   do {
-    array.length = array.length - 1
+    array.length = array.length - 1;
   } while (array.length > 0 && maybeTrue());
 }
