38d24175eef3dbcfdc599a1c9dc1063a2e491125
jaredianmills <jaredianmills@gmail.com>
2018-04-16 20:14:12 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,29 +1,19 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
-      array.push(`I am ${i} strange loop.`);
+      array.push('I am 1 strange loop.')
     } else {
-      array.push(`I am ${i} strange loops.`);
+      array.push(`I am ${i} strange loops.`)
     }
   }
-  return array;
+  return array
 }
 
 function whileLoop(n) {
   while (n > 0) {
-    console.log(--n)
+    console.log(n--);
   }
-  return 'done'
+    console.log('done');
 }
 
-function maybeTrue() {
-  return Math.random() >= 0.5
-}
-
-function doWhileLoop(array) {
-  do {
-    array.length -= 1;
-    return array
-  } while (array.length > 0 && maybeTrue());
-  return array
-}
+whileLoop(parseInt(Math.random() * 100))
