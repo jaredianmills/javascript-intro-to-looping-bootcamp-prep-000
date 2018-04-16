477aeb02433419b1a5870710f7d0d41dea0206a5
jaredianmills <jaredianmills@gmail.com>
2018-04-16 20:20:46 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -20,4 +20,8 @@ function maybeTrue() {
   return Math.random() >= 0.5
 }
 
-function doWhileLoop(array) {}
+function doWhileLoop(array) {
+  do {
+    array.shift()
+  } while array.length > 0 && maybeTrue()
+}
